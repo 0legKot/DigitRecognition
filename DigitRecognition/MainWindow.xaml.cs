82cf0d57c02db95088a5946994bb36e749350c7d
Oleg <kronosgod12@gmail.com>
2018-07-18 15:10:26 +0300

    Getting started

--- DigitRecognition/MainWindow.xaml.cs
+++ DigitRecognition/MainWindow.xaml.cs
@@ -20,9 +20,65 @@ namespace DigitRecognition
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool[,] input = new bool[4, 4];
+        Neuron[] network1 = new Neuron[16];
         public MainWindow()
         {
             InitializeComponent();
+            foreach (Button btn in grdInput.Children)
+            {
+                btn.Background = Brushes.Red;
+                btn.Focusable = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var col = int.Parse(((sender as Button).Content as string)[0].ToString());
+            var row = int.Parse(((sender as Button).Content as string)[1].ToString());
+            input[row, col] = !input[row, col];
+            if (input[row, col]) (sender as Button).Background=Brushes.Green;
+            else (sender as Button).Background = Brushes.Red;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 16; i++)
+                network1[i]=new Neuron() { Value = (input[i / 4, i % 4]) ? 1 : 0 };
+            Neuron UpLine = new Neuron();
+            for (int i = 0; i < 4; i++)
+                UpLine.neuronWithWeights.Add(new NeuronWithWeight() {NeuronItem=network1[i],Weight=0.25 });
+            Neuron DownLine = new Neuron();
+            for (int i = 12; i < 16; i++)
+                DownLine.neuronWithWeights.Add(new NeuronWithWeight() { NeuronItem = network1[i], Weight = 0.25 });
+            Neuron MidLineZ = new Neuron();
+            Neuron MidLineS = new Neuron();
+            for (int i = 4; i < 12; i++)
+            {
+                double weightS = 0.125;
+                double weightZ = 0.125;
+                if (i == 9) weightS = 0.25;
+                if (i == 5) weightZ = -0.125;
+                if (i == 10) weightZ = 0.25;
+                if (i == 6) weightS = -0.125;
+                MidLineS.neuronWithWeights.Add(new NeuronWithWeight() { NeuronItem = network1[i], Weight = weightS });
+                MidLineZ.neuronWithWeights.Add(new NeuronWithWeight() { NeuronItem = network1[i], Weight = weightZ });
+            }
+
+            Neuron Z = new Neuron();
+            Z.neuronWithWeights.Add(new NeuronWithWeight() {NeuronItem = UpLine,Weight=1.0/3 });
+            Z.neuronWithWeights.Add(new NeuronWithWeight() { NeuronItem = MidLineZ, Weight = 1.0 / 3 });
+            Z.neuronWithWeights.Add(new NeuronWithWeight() { NeuronItem = DownLine, Weight = 1.0 / 3 });
+            Neuron S = new Neuron();
+            S.neuronWithWeights.Add(new NeuronWithWeight() { NeuronItem = UpLine, Weight = 1.0 / 3 });
+            S.neuronWithWeights.Add(new NeuronWithWeight() { NeuronItem = MidLineS, Weight = 1.0 / 3 });
+            S.neuronWithWeights.Add(new NeuronWithWeight() { NeuronItem = DownLine, Weight = 1.0 / 3 });
+
+            S.Calculate();
+            Z.Calculate();
+
+            if (Z.Value > S.Value) MessageBox.Show("Z");
+            else MessageBox.Show("S");
         }
     }
 }
---
+++ DigitRecognition/Neuron.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitRecognition
+{
+    class Neuron
+    {
+        public double Value { get; set; }
+        public IList<NeuronWithWeight> neuronWithWeights = new List<NeuronWithWeight>();
+        public void Calculate() {
+            if (neuronWithWeights.Any()) {
+                Value = 0;
+                foreach (NeuronWithWeight n in neuronWithWeights)
+                    n.NeuronItem.Calculate();
+                foreach (NeuronWithWeight n in neuronWithWeights)
+                    Value += n.NeuronItem.Value * n.Weight;
+            }
+        }
+    }
+    class NeuronWithWeight
+    {
+        public Neuron NeuronItem { get; set; }
+        public double Weight { get; set; }
+    }
+}
